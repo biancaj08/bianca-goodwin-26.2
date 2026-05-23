c64e5a3fee4d9e64a946ad60942d23b744bbaadd
biancaj08 <biancajgoodwin@gmail.com>
2026-05-23 14:00:58 -0400

    js added, created footer and skills

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-let footer = document.appendChild("footer");
+let footer = document.createElement("footer");
+document.body.appendChild("footer");
 let today = new Date();
 let thisYear = today.getFullYear();
-let copyright = document.createElement(p);
-copyright.innerHTML = append(thisYear);
+let copyright = document.createElement("p");
+copyright.innerHTML = `Bianca Goodwin ` + thisYear;
 footer.appendChild(copyright);
 
 let skills = ["JavaScript", "HTML", "CSS", "Github"]
 let skillsSelection = document.getElementbyId("skills");
-let skillsList = skillsSelection.getElementByTagName("ul");
+let skillsList = skillsSelection.getElementByTagName("ul")[0];
 
 for (let i = 0; i < skills.length; i++){
-    let skills = document.createElement("li");
-    skills.innerText = skills[i];
-    skillsList[0].appendChild(skill);
-    console.log("new skill", skills);
+    let skillItem = document.createElement("li");
+    skillItem.innerText = skillItem[i];
+    skillsList[0].appendChild(skillItem);
+    console.log("new skill", skillItem);
 }
 
 console.log(skillsList);

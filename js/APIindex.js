5463aa88e0497ba11950004acb58ee3b7a4e3de5
biancaj08 <biancajgoodwin@gmail.com>
2026-06-09 21:19:24 -0400

    added open api page

---
+++ js/APIindex.js
@@ -0,0 +1,54 @@
+fetch('https://api.github.com/users/Startnow08/repos')
+.then(response => {
+    if (!response.ok) {
+        throw new Error("Request failed");
+    }
+    return response.json();
+})
+   
+ .then(data => {
+    console.log("data: ", data);
+    repositories = [...data];
+    console.log("repositories: ", repositories);})
+
+
+const temperature = document.getElementById("temperature result");
+let result = document.getElementById("result");
+let weather;
+let temps;
+
+ const temperatures = fetch('https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=33333c9f0169b63ccdbac11f3b32f09c&units=imperial')
+.then(response => {
+    if (!response.ok) {
+        throw new Error("Request failed");
+    }
+    return response.json();
+}).then(data => {
+    //console.log("data: ", data);
+     return temps = data.main.temp;
+    console.log(temps);  
+
+   
+}
+).finally(temperature.innerHTML = temps);
+const weatherCondition = fetch('https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=33333c9f0169b63ccdbac11f3b32f09c&units=imperial')
+.then(response => {
+    if (!response.ok){
+        throw new Error("Request failed");
+     }
+    return response.json();
+ }).then(data => {
+    let weather = data.main.feels_like;
+    console.log(weather);
+    return weather;
+    
+ }).finally(result.innerHTML = weather)
+
+
+
+
+
+
+
+
+
